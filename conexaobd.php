b06465d285a00da4b4673d655ff4a8695858f8f1
Leonardo Rocha <65306590+leonardorochaa@users.noreply.github.com>
2021-12-26 22:18:23 -0300

    script bd conexão

--- conexaobd.php
+++ conexaobd.php
@@ -1,9 +1,11 @@
 <?php
 
+// Variáveis de conexão do banco de dados
 define($dbHost='localhost');
 define($dbUsername='root');
 define($dbPass='123qwe');
 define($dbName='test');
 
+// Inicia conexão com o banco de dados
 $conexao = new mysqli($dbHost, $dbUsername, $dbPass, $dbName);
 
