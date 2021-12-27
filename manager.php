74d1c31b73cc591641858435a72fa106f8dee801
Leonardo Rocha <65306590+leonardorochaa@users.noreply.github.com>
2021-12-26 22:21:48 -0300

    script manager

--- manager.php
+++ manager.php
@@ -1,11 +1,13 @@
 <?php
 
+// Abre conexão com o banco de dados
 include_once('conexaobd.php');
 
+// Consulta tabela manager
 $sql = "SELECT * from manager";
 $result = $conexao->query($sql);
 
-
+// Trás os resultados de cada coluna salvando em uma variável
 if($result->num_rows > 0)
 {
     while($row = mysqli_fetch_assoc($result))
@@ -22,3 +24,4 @@ else
     echo "Falha ao conectar no manager!";
 }
 
+?>
