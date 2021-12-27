29247e330ccae63f1a40d869b2eac98621fd735d
Leonardo Rocha <65306590+leonardorochaa@users.noreply.github.com>
2021-12-26 22:19:43 -0300

    Página Inicial

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
+<html>
 <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<!------ Include the above in your HEAD tag ---------->
 
 <style>
     body {
@@ -57,3 +57,4 @@
     </div>
 </div>
 </body>
+</html>
